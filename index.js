ef7d1333ce9267181b782de7b4b380bc33cb3be3
Laura Kim <Doyeunkim120@gmail.com>
2022-02-12 04:24:45 -0800

    pacman created and gave function to move around

--- index.js
+++ index.js
@@ -25,8 +25,11 @@ const gridLayout = [
 //console.log(gridLayout.length);
 
 const container = document.querySelector(".container");
-const score = document.querySelector(".score");
 const squares = [];
+
+const score = document.querySelector(".score");
+const currentScore = 0;
+
 const width = 285;
 const height = 315;
 
@@ -50,3 +53,50 @@ function createSquares () {
 
 createSquares();
 //console.log(squares);
+
+//create pacman in the middle when the game starts: index of 180.        
+let position = 180;            
+squares[180].classList.remove("pac-dot");
+squares[180].classList.add("pac-man");
+
+
+//allow player to move pacman:
+function move(event) {
+     let keyPressed = event.key;
+     squares[position].classList.remove("pac-man");
+
+    switch (keyPressed) {
+        case "ArrowDown":
+            if (position < 399) {
+            position += 19;
+            squares[position].classList.remove("pac-dot");
+            squares[position].classList.add("pac-man");
+            }
+            break;
+        case "ArrowUp":
+            if (position >= 19) {
+            position -= 19;
+            squares[position].classList.remove("pac-dot");
+            squares[position].classList.add("pac-man");
+            }
+            break;
+        case "ArrowLeft":
+            if (position % 19 !== 0) {
+            position -= 1;
+            squares[position].classList.remove("pac-dot");
+            squares[position].classList.add("pac-man");
+            }
+            break;
+        case "ArrowRight":
+            if (position % 19 <  18) {
+            position += 1;
+            squares[position].classList.remove("pac-dot");
+            squares[position].classList.add("pac-man");
+            }
+            break;
+    }
+ }
+
+document.addEventListener("keyup", move);
+
+//Next: need to make sure pacmand does not move if there is a wall or an empty space
